4a7056b743d4248de9fa976385e010fcd2d02258
Pranay M. Bobade <71088435+pranaybobde@users.noreply.github.com>
2024-04-10 19:18:03 +0530

    UpdateD Main.py

--- Main.py
+++ Main.py
@@ -56,5 +56,5 @@ async def predict(
             'confidence': float(confidence)
         }
 
-if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host='localhost', port=8000)
